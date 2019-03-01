2110bbbf7d255b46c26abb7125938b948b4eeb85
Brian Wang <brrian@gmail.com>
2019-02-28 19:34:45 -0600

    Update parent transaction when deleting split

--- src/schemas/Split/resolver.ts
+++ src/schemas/Split/resolver.ts
@@ -1,13 +1,24 @@
+import { Context } from '../..';
 import db from '../../models';
 import { updateStashIfNeeded } from '../Transaction/helpers';
 import { MutateDeleteSplit, MutateToggleSplit } from './typeDef';
 
 export default {
   Mutation: {
-    async deleteSplit(_: any, { id }: MutateDeleteSplit) {
-      const destroyedRows = await db.Split.destroy({ where: { id } });
+    async deleteSplit(_: any, { id }: MutateDeleteSplit, { groupId }: Context) {
+      const split = await db.Split.findByPk(id, { include: [db.Transaction] });
 
-      return destroyedRows === 1;
+      if (!split || split.Transaction.groupId !== groupId) {
+        throw new Error('Split does not exist by that id');
+      }
+
+      await split.Transaction.update({
+        amount: split.Transaction.amount + split.amount,
+      });
+
+      await split.destroy();
+
+      return true;
     },
 
     async toggleSplit(_: any, { id }: MutateToggleSplit) {
